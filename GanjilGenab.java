5940515b2d8ee1d0059ac54bf65c25fc51f6355d
muhamadzain34rpl-ui <muhamad_zain_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 13:48:32 +0700

    Add files via upload

---
+++ GanjilGenab.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GanjilGenab {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("masukkan sebuah bilangan: ");
+        int bil=input.nextInt();
+        if(bil%2==0){
+            System.out.println(bil+" adalah bilangan genap");
+        }
+        else{
+            System.out.println(bil+" adalah bilangan ganjil");
+   }}
+}
+
